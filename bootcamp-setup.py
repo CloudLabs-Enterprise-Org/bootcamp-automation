6bd85cd3e61b5fa727495c2eed39d067a59a5d23
cloudlabs-enterprise <161740101+cloudlabs-enterprise@users.noreply.github.com>
2024-05-07 03:45:39 +0530

    Update bootcamp-setup.py

--- bootcamp-setup.py
+++ bootcamp-setup.py
@@ -4,31 +4,32 @@ import os
 import logging
 import yaml
 import time
+import requests
+from datetime import datetime
 
+import logging
+
+# Configure logging to write to a file
+logging.basicConfig(filename='script_log.log', level=logging.DEBUG)
+# Log script start
+logging.info('Script started')
+
+print("Number of arguments:", len(sys.argv))
+print("Argument list:", str(sys.argv))
 # Get Arguments
-
-working_repo = sys.argv[1]
-
-issue_num = sys.argv[2]
-
-
+working_repo = 'CloudLabs-Enterprise-Org/bootcamp-automation'
+issue_num = '43'
+githubuser = sys.argv[1]
 
 # Get Environment Variables
-
-github_token = os.environ.get("GITHUB_TOKEN")
-
-admin_token = os.environ.get("ADMIN_TOKEN")
+github_token = sys.argv[2] 
+admin_token = sys.argv[3] 
 
 # Setup clients
 issue_ops_client = client.Client(github_token, working_repo, issue_num)
-admin_client = client.Client(admin_token)
-
-logging.info("Starting bootcamp setup")
-logging.info(f"Working repo: {working_repo}")
-logging.info(f"Issue number: {issue_num}")
+admin_client = client.Client(admin_token) 
 
 
-# Read config file
 def get_config(config_file):
     with open(config_file, "r") as stream:
         try:
@@ -37,8 +38,6 @@ def get_config(config_file):
             logging.error(e)
             sys.exit(1)
 
-
-# Get bootcamp date, attendees, and facilitators from issue body
 def extract_issue_fields():
     try:
         issue = issue_ops_client.issue.get()
@@ -47,37 +46,14 @@ def extract_issue_fields():
     except Exception as e:
         raise e
 
-    bootcamp_date = None
-    attendee_handles = None
-    facilitator_handles = None
+    bootcamp_date = datetime.now().strftime('%Y-%m-%d')
+    attendee_handles = [githubuser,]
+    facilitator_handles = ['cloudlabs-enterprise',]
 
-    for i, line in enumerate(lines):
-        if line == "### Bootcamp Date":
-            bootcamp_date = lines[i + 2].strip()
-        elif line == "### Attendees":
-            attendee_handles = [
-                attendee.strip() for attendee in lines[i + 2].split(",")
-            ]
-        elif line == "### Facilitators":
-            facilitator_handles = [
-                facilitator.strip() for facilitator in lines[i + 2].split(",")
-            ]
 
-    if not bootcamp_date:
-        raise ValueError("Bootcamp date cannot be empty")
-    if not attendee_handles:
-        raise ValueError("Attendees cannot be empty")
-    if not facilitator_handles:
-        raise ValueError("Facilitators cannot be empty")
-
-    logging.info(f"Bootcamp date: {bootcamp_date}")
-    logging.info(f"Attendees: {attendee_handles}")
-    logging.info(f"Facilitators: {facilitator_handles}")
 
     return bootcamp_date, attendee_handles, facilitator_handles
 
-
-# Attendee state factory - builds a list of attendees with their initial state
 def build_attendees(handles):
     attendees = []
     for handle in handles:
@@ -95,43 +71,77 @@ def build_attendees(handles):
             raise e
 
         attendees.append(attendee)
-
+        print(attendees)
     return attendees
 
 
-# Create orgs and fork repos
-def provision_enironments(
-    attendee_state, config, enterprise_id, bootcamp_date, facilitator_state
-):
+
+def get_organization_info(org_name):
+    headers = {
+        'Authorization': 'token ${admin_token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    url = f'https://api.github.com/orgs/{org_name}'
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        org_info = response.json()
+        org_id = org_info.get('id')
+        org_login = org_info.get('login')
+        print(f"Organization ID: {org_id}")
+        print(f"Organization login: {org_login}")
+        return org_id, org_login
+    else:
+        print(f"Failed to fetch organization info for {org_name}: {response.status_code} - {response.text}")
+        return None, None
+
+def provision_environments(attendee_state, config, enterprise_id, bootcamp_date, facilitator_state):
     facilitator_handles = [facilitator["handle"] for facilitator in facilitator_state]
     facilitator_handles.append(config["billing-admin"])
 
-    # Create orgs and fork repos
     for attendee in attendee_state:
         try:
-            # If an org_name is too long because the attendee handle is long, only get the first 39 characters
-            org_name = (
-                config["org-prefix"] + "-" + bootcamp_date + "-" + attendee["handle"]
-            )
+            handle_parts = attendee["handle"].split('-')
+            last_part = handle_parts[-1]  # Selecting the last part of the split string
+            org_name = config["org-prefix"] + "-" + bootcamp_date + "-" + "cloudlabs" + last_part
             if len(org_name) > 39:
-                org_name = org_name[0:38]
+                org_name = org_name[:38]
 
+            # Create organization
             org_id, org_name = admin_client.org.create(
                 enterprise_id,
                 org_name,
                 facilitator_handles,
                 f"{config['billing-admin']}@spektrasystems.com",
             )
-            attendee.update({"org_id": org_id, "org_name": org_name})
-        except Exception:
-            pass
+            
+            # Fetch organization information
+            org_id, org_login = get_organization_info(org_name)
 
+            if org_id and org_login:
+                attendee.update({"org_id": org_id, "org_name": org_login})
+                print(attendee)
+        except Exception as e:
+            print(f"Error provisioning environment for {attendee['handle']}: {e}")
+
+    return attendee_state, org_name
+
+
+def fork_repo(
+    attendee_state, config, facilitator_state, org_name
+):
+    facilitator_handles = [facilitator["handle"] for facilitator in facilitator_state]
+    facilitator_handles.append(config["billing-admin"])
+
+    # Create orgs and fork repos
+    for attendee in attendee_state:
+        
         for repo in config["repos-to-fork"]:
             try:
-                admin_client.repo.fork(repo, attendee["org_name"])
+                admin_client.repo.fork(repo, org_name)
                 # Make forked repos private, except for .github
                 if repo.split("/")[1] != ".github":
-                    forked_repo = attendee["org_name"] + "/" + repo.split("/")[1]
+                    forked_repo = org_name + "/" + repo.split("/")[1]
                     # Adding a sleep to avoid race condition when creating a repo and marking it private
                     time.sleep(15)
                     admin_client.repo.visibility(forked_repo, "private")
@@ -141,102 +151,59 @@ def provision_enironments(
 
     return attendee_state
 
-
 def main():
     # Get config
     config = get_config("config.yml")
     for key, value in config.items():
         logging.info(f"{key}: {value}")
-
-    # apply starting label
-    issue_ops_client.issue.apply_label(config["labels"]["working"])
-    issue_ops_client.issue.remove_label(config["labels"]["new"])
-
-    # Get info from issue
     try:
         bootcamp_date, attendee_handles, facilitator_handles = extract_issue_fields()
+        print(bootcamp_date)
+        print(attendee_handles)
+        print(facilitator_handles)
     except Exception as e:
-        issue_ops_client.issue.apply_label(config["labels"]["error"])
-        issue_ops_client.issue.remove_label(config["labels"]["working"])
-        issue_ops_client.issue.add_comment(comments.errored)
-        issue_ops_client.issue.close()
+        logging.error(e)
         sys.exit(1)
-
-    # Get enterprise id
+    #print(build_attendees)
     try:
         enterprise_id = admin_client.enterprise.get_id(config["enterprise"])
+        print(enterprise_id)
     except Exception as e:
-        issue_ops_client.issue.apply_label(config["labels"]["error"])
-        issue_ops_client.issue.remove_label(config["labels"]["working"])
-        issue_ops_client.issue.add_comment(comments.errored)
-        issue_ops_client.issue.close()
+        logging.error(e)
         sys.exit(1)
-
-    # Build attendee list
     try:
         attendee_state = build_attendees(attendee_handles)
         facilitator_state = build_attendees(facilitator_handles)
+        print(attendee_state)
+        print(facilitator_state)
     except Exception as e:
-        # TODO: Comment and close issue
-        issue_ops_client.issue.apply_label(config["labels"]["error"])
-        issue_ops_client.issue.remove_label(config["labels"]["working"])
-        issue_ops_client.issue.add_comment(comments.errored)
-        issue_ops_client.issue.close()
+        logging.error(e)
         sys.exit(1)
 
-    # Create bootcamp orgs
-    attendee_state = provision_enironments(
+  
+    attendee_state, org_name = provision_environments(
         attendee_state, config, enterprise_id, bootcamp_date, facilitator_state
     )
+    print(attendee_state)
 
-    facilitator_state = provision_enironments(
-        facilitator_state, config, enterprise_id, bootcamp_date, facilitator_state
+    attendee_state = fork_repo(
+        attendee_state, config,  facilitator_state , org_name
     )
+    print(attendee_state)
 
-    # Wait for orgs to be created
-    wait_time = 30
-    logging.info(f"Waiting {wait_time} seconds for orgs to be created")
-    time.sleep(wait_time)
-
-    # invite attendees to orgs
     error_count = 0
     for attendee in attendee_state:
-        if attendee["org_name"]:
+        if org_name:
             try:
-                admin_client.org.invite_member(attendee["id"], attendee["org_name"])
+                admin_client.org.invite_member(attendee["id"], org_name)
                 attendee["invited"] = True
-            except Exception:
-                issue_ops_client.issue.apply_label(config["labels"]["error"])
+            except Exception as e:
+                logging.error(e)
+                sys.exit(1)
         else:
             error_count += 1
 
-    # Check for provisioning errors
-    if error_count > 0:
-        issue_ops_client.issue.apply_label(config["labels"]["error"])
-        issue_ops_client.issue.remove_label(config["labels"]["working"])
-        comment = (
-            comments.errored
-            + "### Attendees\n\n"
-            + comments.attendees_to_markdown(attendee_state)
-            + "### Facilitators\n\n"
-            + comments.attendees_to_markdown(facilitator_state)
-        )
-        issue_ops_client.issue.add_comment(comment)
-        issue_ops_client.issue.close()
-        sys.exit(1)
-
-    # Update the issue with the results
-    issue_ops_client.issue.apply_label(config["labels"]["done"])
-    issue_ops_client.issue.remove_label(config["labels"]["working"])
-    comment = (
-        comments.complete
-        + "### Attendees\n\n"
-        + comments.attendees_to_markdown(attendee_state)
-        + "### Facilitators\n\n"
-        + comments.attendees_to_markdown(facilitator_state)
-    )
-    issue_ops_client.issue.add_comment(comment)
-
-
 if __name__ == "__main__":
     main()
+# Log script end
+logging.info('Script finished')
